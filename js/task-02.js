0b68eb025fd33fc1f11bfac7de3f329e1dfc5eb1
Adrian Lacheta <adiczq@gmail.com>
2023-03-11 15:36:12 +0100

    1

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingrEl = document.getElementById("ingredients");
+console.log(ingrEl);
+
+const items = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+
+ingrEl.append(...items);
