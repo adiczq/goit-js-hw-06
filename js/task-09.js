05c64fa61f22dadc4a7e4ddc1c61ae19347d1eab
Adrian Lacheta <adiczq@gmail.com>
2023-03-26 17:16:42 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
-function getRandomHexColor() {
+const changeBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const text = document.querySelector(".color");
+
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const colorChange = () => {
+  text.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+};
+
+changeBtn.addEventListener("click", colorChange);
