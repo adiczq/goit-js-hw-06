d25f80fb5d01f6017e768a1cfca0ea66c4f9e511
Adrian Lacheta <adiczq@gmail.com>
2023-03-26 17:00:54 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", submit);
 
+function submit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Wypełnij wszystkie pola");
+  }
+  console.log(`Email: ${email.value} , password: ${password.value}`);
+  e.currentTarget.reset();
+}
